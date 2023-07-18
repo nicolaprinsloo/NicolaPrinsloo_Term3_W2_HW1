e9a771a6e4ed0c1e0b4252a7f17fbbcfa0702b43
nicolaprinsloo <231021@virtualwindow.co.za>
2023-07-18 12:31:56 +0200

    own ex 2

--- js/test.js
+++ js/test.js
@@ -1,9 +1,20 @@
-$(document).ready(function () {
-    $("h1").click(function (){
-        $("h1").hide();
-    });
-    $("#imgButton").click(function(){
-        $("img").slideUp(2000).slideDown(2000);
-    })
-});
+// $(document).ready(function () {
+//     $("h1").click(function (){
+//         $("h1").hide();
+//     });
+//     $("#imgButton").click(function(){
+//         $("img").slideUp(2000).slideDown(2000);
+//     })
+// });
 
+$(document).ready(function() {
+    $("h2").dblclick(function() {
+        $("h2").slideUp(2000).slideDown(2000)
+    });
+    $("#learnBtn").click(function(){
+        $("img").slideUp(2000)
+    });
+    $("#backBtn").click(function(){
+        $("img").slideDown(2000)
+    });
+});
